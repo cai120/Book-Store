1bb4dc95b748fc1ca74283ce4b68ffe879871789
cai_wing <cai.wing@guestia.co.uk>
2022-12-16 12:51:50 +0000

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+        {
+            public ApplicationDbContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+                optionsBuilder.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=Bulky;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+                return new ApplicationDbContext(optionsBuilder.Options);
+            }
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
